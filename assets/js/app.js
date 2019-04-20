a648ad275175dcbc060357c089cdd34f1cd5fa1e
StewartRussell911 <49731764+StewartRussell911@users.noreply.github.com>
2019-04-20 21:32:21 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -323,12 +323,14 @@ function buildConfig() {
 // Basemap Layers
 var mapquestOSM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
   maxZoom: 19,
+  subdomains: ["otile1"],
   attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
 });
 
 
 var MapLyr = L.tileLayer('https://api.tiles.mapbox.com/v4/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
+		subdomains: ["otile1"],
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
 });
