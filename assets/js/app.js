0825233600fbad75a1bacceeb9e6457f54f86b8e
StewartRussell911 <49731764+StewartRussell911@users.noreply.github.com>
2019-04-22 22:06:02 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 var config = {
   geojson: "./data/congress_park_trees.geojson",
-  title: "Park Trees - Dashboard 2",
+  title: "Park Trees - Dashboard 3",
   layerName: "Trees",
   hoverProperty: "species_sim",
   sortProperty: "dbh_2012_inches_diameter_at_breast_height_46",
