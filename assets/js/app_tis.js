f13da4e506dfafc8bbc4825e1e1ff3bef0de723d
StewartRussell911 <49731764+StewartRussell911@users.noreply.github.com>
2019-05-02 15:56:36 +0200

    Update app_tis.js

--- assets/js/app_tis.js
+++ assets/js/app_tis.js
@@ -1,6 +1,6 @@
 var config = {
   geojson: "./data/count_station_totals.geojson",
-  title: "RRAMS Viewer > Counts 2",
+  title: "RRAMS Viewer > Counts 3",
   layerName: "Overberg DM Counts",
   hoverProperty: "tcname",
   sortProperty: "count_date",
@@ -93,7 +93,7 @@ var properties = [{
   }
 },
 {
-  value: "oberg_inventory_custodia",
+  value: "oberg_inventory_custodian",
   label: "Custodian",
   table: {
     visible: true,
